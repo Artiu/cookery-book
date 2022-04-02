1e8973d3c5468096c96649dfe87d97a08eb5ed5b
Artiu <71273256+Artiu@users.noreply.github.com>
2022-04-02 14:01:20 +0200

    Add saving query to sessionStorage

--- features/recipe/hooks/useSearchRecipe.jsx
+++ features/recipe/hooks/useSearchRecipe.jsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function useSearchRecipe(list) {
     const [query, setQuery] = useState("");
     const filteredList = list.filter((item) =>
         item.title.toLowerCase().includes(query.toLowerCase())
     );
+    useEffect(() => {
+        setQuery(sessionStorage.getItem("query") || "");
+    }, []);
+    useEffect(() => {
+        sessionStorage.setItem("query", query);
+    }, [query]);
     return { filteredList, query, setQuery };
 }
