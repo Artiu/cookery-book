2008f85ab033f3258206f335b215e8cf19974d2a
Artiu <oborskiartur123@gmail.com>
2024-04-06 14:19:51 +0200

    Fix remove feature and add scaling images before upload for faster loading website

--- features/recipe/ui/RemoveButton.jsx
+++ features/recipe/ui/RemoveButton.jsx
@@ -1,68 +1,70 @@
 import { useState } from "react";
 import {
-    AlertDialog,
-    AlertDialogBody,
-    AlertDialogContent,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogOverlay,
-    Button,
-    useDisclosure,
+	AlertDialog,
+	AlertDialogBody,
+	AlertDialogContent,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogOverlay,
+	Button,
+	useDisclosure,
 } from "@chakra-ui/react";
 import { deleteDoc, doc } from "firebase/firestore";
 import { deleteObject, ref } from "firebase/storage";
-import { FIRESTORE } from "init/firebase";
+import { FIREBASE_STORAGE, FIRESTORE } from "init/firebase";
 import { useRouter } from "next/router";
 import { revalidate } from "shared/revalidate";
 import useMyToast from "shared/hooks/useMyToast";
 
 export default function RemoveButton({ recipe }) {
-    const router = useRouter();
-    const toast = useMyToast();
-    const [isRemoving, setIsRemoving] = useState(false);
-    const removeRecipe = async () => {
-        setIsRemoving(true);
-        try {
-            await deleteDoc(doc(FIRESTORE, "recipes", recipe.id));
-            if (recipe.withImage) {
-                await deleteObject(ref(`images/${recipe.id}`));
-            }
-            await revalidate(`/recipe/${recipe.id}`);
-            await revalidate("/");
-        } catch {
-            toast({ type: "error", description: "Coś poszło nie tak!" });
-        }
-        setIsRemoving(false);
-        router.push("/");
-        router.reload();
-    };
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    return (
-        <>
-            <AlertDialog isOpen={isOpen} onClose={onClose}>
-                <AlertDialogOverlay>
-                    <AlertDialogContent>
-                        <AlertDialogHeader fontSize="lg" fontWeight="bold">
-                            Usuń przepis
-                        </AlertDialogHeader>
+	const router = useRouter();
+	const toast = useMyToast();
+	const [isRemoving, setIsRemoving] = useState(false);
+	const removeRecipe = async () => {
+		setIsRemoving(true);
+		try {
+			await deleteDoc(doc(FIRESTORE, "recipes", recipe.id));
+			if (recipe.withImage) {
+				await deleteObject(ref(FIREBASE_STORAGE, `images/${recipe.id}`));
+			}
+			await revalidate(`/recipe/${recipe.id}`);
+			await revalidate("/");
+			l;
+		} catch (err) {
+			console.error(err);
+			toast({ type: "error", description: "Coś poszło nie tak!" });
+		}
+		setIsRemoving(false);
+		router.push("/");
+		router.reload();
+	};
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	return (
+		<>
+			<AlertDialog isOpen={isOpen} onClose={onClose}>
+				<AlertDialogOverlay>
+					<AlertDialogContent>
+						<AlertDialogHeader fontSize="lg" fontWeight="bold">
+							Usuń przepis
+						</AlertDialogHeader>
 
-                        <AlertDialogBody>Czy napewno chcesz usunąć przepis?</AlertDialogBody>
+						<AlertDialogBody>Czy napewno chcesz usunąć przepis?</AlertDialogBody>
 
-                        <AlertDialogFooter>
-                            <Button onClick={onClose}>Anuluj</Button>
-                            <Button
-                                colorScheme="red"
-                                onClick={removeRecipe}
-                                ml={3}
-                                isLoading={isRemoving}
-                            >
-                                Usuń
-                            </Button>
-                        </AlertDialogFooter>
-                    </AlertDialogContent>
-                </AlertDialogOverlay>
-            </AlertDialog>
-            <Button onClick={onOpen}>Usuń</Button>
-        </>
-    );
+						<AlertDialogFooter>
+							<Button onClick={onClose}>Anuluj</Button>
+							<Button
+								colorScheme="red"
+								onClick={removeRecipe}
+								ml={3}
+								isLoading={isRemoving}
+							>
+								Usuń
+							</Button>
+						</AlertDialogFooter>
+					</AlertDialogContent>
+				</AlertDialogOverlay>
+			</AlertDialog>
+			<Button onClick={onOpen}>Usuń</Button>
+		</>
+	);
 }
--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,10 +1,10 @@
 const handler = async (req, res) => {
-    try {
-        await res.unstable_revalidate(JSON.parse(req.body).path);
-        res.json({ revalidated: true });
-    } catch (err) {
-        console.error(err);
-        res.json({ revalidated: false });
-    }
+	try {
+		await res.revalidate(JSON.parse(req.body).path);
+		res.json({ revalidated: true });
+	} catch (err) {
+		console.error(err);
+		res.json({ revalidated: false });
+	}
 };
 export default handler;
